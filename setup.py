5edf1959023abe9bbd04b0ce55ef2bac31a70687
ifsale <irony00100@gmail.com>
2022-09-27 05:32:46 +0700

    initial commit

--- setup.py
+++ setup.py
@@ -4,24 +4,20 @@ import os
 import re
 from setuptools import setup, find_packages
 
-PROJECT_DIR = os.path.dirname( os.path.realpath( __file__) )
-
-def _load_readme( ):
-	readme_path = os.path.join( PROJECT_DIR, 'README.md' )
-	with codecs.open( readme_path, 'r', 'utf-8' ) as f:
-		return f.read( )
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 setup(
 	name='grow-room',
-	version='1.0.0',
+	version='1.0',
 	description='Grow room app',
-	long_description=_load_readme( ),
 	author='Irony',
 	author_email='carlo@ground-creative.com',
+	#packages=['grow-room'],  #same as name
 	install_requires=[ 
 		'coloredlogs', 
 		'logging', 
 		'paho-mqtt', 
 		'tuyalinksdk' 
-	]
+	]#, #external packages as dependencies
+	#python_requires='>=3.6'
 )
